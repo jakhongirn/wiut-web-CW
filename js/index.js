e1a469474180e9ad22efed5bea539eb0ab0476c3
jaykhans <jahongirnagmatov@gmail.com>
2021-12-09 23:43:09 +0500

    ad js comments

--- js/index.js
+++ js/index.js
@@ -5,8 +5,8 @@ var TxtRotate = function (el, toRotate, period) {
   this.loopNum = 0;
   this.period = parseInt(period, 10) || 2000;
   this.txt = "";
-  this.tick();
-  this.isDeleting = false;
+  this.tick();  //period of rotate in typing
+  this.isDeleting = false; 
 };
 
 TxtRotate.prototype.tick = function () {
@@ -14,12 +14,12 @@ TxtRotate.prototype.tick = function () {
   var fullTxt = this.toRotate[i];
 
   if (this.isDeleting) {
-    this.txt = fullTxt.substring(0, this.txt.length - 1);
+    this.txt = fullTxt.substring(0, this.txt.length - 1); 
   } else {
-    this.txt = fullTxt.substring(0, this.txt.length + 1);
+    this.txt = fullTxt.substring(0, this.txt.length + 1); //add each letter or removes depending on isDeleting is true or false
   }
 
-  this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
+  this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>"; //type below when reaches the limit of width of container
 
   var that = this;
   var delta = 200 - Math.random() * 100;
@@ -34,7 +34,7 @@ TxtRotate.prototype.tick = function () {
   } else if (this.isDeleting && this.txt === "") {
     this.isDeleting = false;
     this.loopNum++;
-    delta = 500;
+    delta = 500; //when types all of the words it will get back and removes letter one by one and vice versa. 
   }
 
   setTimeout(function () {
@@ -45,9 +45,9 @@ TxtRotate.prototype.tick = function () {
 window.onload = function () {
   var elements = document.getElementsByClassName("txt-rotate");
   for (var i = 0; i < elements.length; i++) {
-    var toRotate = elements[i].getAttribute("data-rotate");
+    var toRotate = elements[i].getAttribute("data-rotate"); //takes each data in the data-rotate attribute list
 
-    var period = elements[i].getAttribute("data-period");
+    var period = elements[i].getAttribute("data-period"); //takes data in the data-period attribute
     if (toRotate) {
       new TxtRotate(elements[i], JSON.parse(toRotate), period);
     }
@@ -55,22 +55,22 @@ window.onload = function () {
   // INJECT CSS
   var css = document.createElement("style");
   css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #1b5bf7 }";
-  document.body.appendChild(css);
+  document.body.appendChild(css); //shows the | to untype or type
 };
 
 // Number animation
-const counters = document.querySelectorAll(".value");
+const counters = document.querySelectorAll(".value"); //takes the all elements which class is value
 const speed = 300;
 
 counters.forEach((counter) => {
   const animate = () => {
-    const value = +counter.getAttribute("data");
+    const value = +counter.getAttribute("data"); //attribute data represents the maximum number
     const data = +counter.innerText;
 
     const time = value / speed;
     if (data < value) {
       counter.innerText = Math.ceil(data + time);
-      setTimeout(animate, 1);
+      setTimeout(animate, 1);  //maximum number divides into speed and adds 1 to 0 (starter) in each that speed and loops until gets maxmimum number data
     } else {
       counter.innerText = value;
     }
@@ -81,8 +81,8 @@ counters.forEach((counter) => {
 
 function myMap() {
   var mapProp = {
-    center: new google.maps.LatLng(51.508742, -0.12085),
-    zoom: 5,
+    center: new google.maps.LatLng(41.2995, 69.2401), //shows the area of Tashkent in google maps
+    zoom: 10,
   };
   var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
 }
